86b647332fb550bbd4b3d61b4ae0d50a7e384516
FahimSifnatul <sifnatul2475@gmail.com>
2021-08-05 14:24:46 +0600

    fixed error message show

--- static/js/index.js
+++ static/js/index.js
@@ -14,7 +14,12 @@ $(document).ready(function(){
 
 	// Sign Up form submit - Actions 
 	$("#form_sign_up").on('submit', function(e){
-		
+
+		// To remove previous errors
+		$("#form_sign_up_error_parent").empty();
+		$("#form_sign_up_error_parent").append("<div id='form_sign_up_error' \
+			class='form-control-text alert alert-danger alert-dismissible' style='display: none;'>\
+                  </div>");
 
 		const password = $("#new_password").val();
 		const new_password = $("#new_confirm_password").val();
@@ -22,6 +27,7 @@ $(document).ready(function(){
 		if(password != new_password)
 		{
 			e.preventDefault();
+			// To add new error
 			$("#form_sign_up_error").html("Passwords mismatch");
 			$("#form_sign_up_error").append("<button type='button' class='close' data-dismiss='alert'>&times;</button>");
 			$("#form_sign_up_error").show("fast");
@@ -29,6 +35,7 @@ $(document).ready(function(){
 		else if(password.length < 8)
 		{
 			e.preventDefault();
+			// To add new error
 			$("#form_sign_up_error").html("Password is too short. \
 				Choose a password between 8-16 characters containing a numeric, \
 				a letter and special character.");
@@ -38,6 +45,7 @@ $(document).ready(function(){
 		else if(password.length > 16)
 		{
 			e.preventDefault();
+			// To add new error
 			$("#form_sign_up_error").html("Password is too long. \
 				Choose a password between 8-16 characters containing atleast one from numeric, \
 				letter and special character each.");
@@ -49,6 +57,7 @@ $(document).ready(function(){
 						/[!-/]|[:-@]|[[-`]|[{-~]/.test(password) == false) // JS Regex to find numeric, letter, special character
 		{
 			e.preventDefault();
+			// To add new error
 			$("#form_sign_up_error").html("Missing atleast one numeric, letter and special character. \
 				Choose a password between 8-16 characters containing atleast one from numeric, \
 				letter and special character each.");
